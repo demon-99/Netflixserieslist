5b2291e4b143688f526f71ec9b7a47fc02cdbb62
nikhil <nikhilk3023@gmail.com>
2021-03-03 10:44:24 +0530

    added css

---
+++ src/Sdata.jsx
@@ -0,0 +1,29 @@
+const Sdata=[
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg" ,
+        title:"A netflix original series",
+        sname:"Dark"
+    },
+    {
+        imgsrc:"https://ftw.usatoday.com/wp-content/uploads/sites/90/2019/04/friends-week.jpg" ,
+        title:"A netflix original series" ,
+        sname:"Friends",
+    },
+    {
+        imgsrc:"https://sm.mashable.com/t/mashable_in/photo/default/never-have-i-ever-review-mindy-kalings-netflix-teen-comedy-g_53wh.960.jpg", 
+        title:"A netflix original series", 
+        sname:"Never have i ever"
+    },
+    {
+        imgsrc:"https://cdn.lifestyleasia.com/wp-content/uploads/sites/7/2020/10/06150020/mirzapur-season-2-1200-1-min.jpg",
+        title:"A netflix original series",
+        sname:"Mirzapur"
+    },
+    {
+       imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdpfB8-KzWatz4XX30xWuaKwV41smz147cmw&usqp=CAU",
+       title:"A netflix original series",
+       sname:"mismatched" 
+    }
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './Card';
+import "./index.css";
+import Sdata from "./Sdata";
 
 
 ReactDOM.render(
 <>
-<Card imgsrc="https://wallpapercave.com/wp/wp4056410.jpg" title="A netflix original series" sname="Dark" />
-<Card imgsrc="https://ftw.usatoday.com/wp-content/uploads/sites/90/2019/04/friends-week.jpg" title="A netflix original series" sname="Friends" />
-<Card imgsrc="https://sm.mashable.com/t/mashable_in/photo/default/never-have-i-ever-review-mindy-kalings-netflix-teen-comedy-g_53wh.960.jpg" title="A netflix original series" sname="Never have i ever" />
+<h1 className="heading_style"> List of top 5 netflix series in 2021</h1>
+<Card imgsrc={Sdata[0].imgsrc} title={Sdata[0].title} sname={Sdata[0].sname} />
+<Card imgsrc={Sdata[1].imgsrc} title={Sdata[1].title} sname={Sdata[1].sname} />
+<Card imgsrc={Sdata[2].imgsrc} title={Sdata[2].title} sname={Sdata[2].sname} />
+<Card imgsrc={Sdata[3].imgsrc} title={Sdata[3].title} sname={Sdata[3].sname} />
+<Card imgsrc={Sdata[4].imgsrc} title={Sdata[4].title} sname={Sdata[4].sname} />
 </>,
 document.getElementById('root'));
