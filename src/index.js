64cb6b800030128c59ea1df0300695d7568f7596
nikhil <nikhilk3023@gmail.com>
2021-03-04 11:21:04 +0530

    added map js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import "./index.css";
 import Sdata from "./Sdata";
 
 
+function ncard(val){
+    return(
+        <Card 
+        imgsrc={val.imgsrc}
+        title={val.title}
+        sname={val.sname}
+        />
+    );
+}
+
 ReactDOM.render(
 <>
 <h1 className="heading_style"> List of top 5 netflix series in 2021</h1>
-<Card imgsrc={Sdata[0].imgsrc} title={Sdata[0].title} sname={Sdata[0].sname} />
-<Card imgsrc={Sdata[1].imgsrc} title={Sdata[1].title} sname={Sdata[1].sname} />
-<Card imgsrc={Sdata[2].imgsrc} title={Sdata[2].title} sname={Sdata[2].sname} />
-<Card imgsrc={Sdata[3].imgsrc} title={Sdata[3].title} sname={Sdata[3].sname} />
-<Card imgsrc={Sdata[4].imgsrc} title={Sdata[4].title} sname={Sdata[4].sname} />
+{Sdata.map(ncard)}
 </>,
 document.getElementById('root'));
